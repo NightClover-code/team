370d59fe6a122a088a028a63170d9ddfe89a4622
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-08-29 22:31:42 +0100

    added hero section content

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,12 +1,19 @@
 const Hero = () => {
   return (
     <section className="hero__section">
-      <h1>Instant collaboration for remote teams</h1>
-      <p></p>
-      <form className="newsletter__form">
-        <input type="email" />
-        <button className="primary__button"></button>
-      </form>
+      <div className="container">
+        <div className="hero__content">
+          <h1>Instant collaboration for remote teams</h1>
+          <p>
+            The easiest and fastest way to share work with groups and to
+            simplify project management.
+          </p>
+          <form className="newsletter__form">
+            <input type="email" placeholder="Email" />
+            <button className="primary__button">Get Early Access</button>
+          </form>
+        </div>
+      </div>
     </section>
   );
 };
