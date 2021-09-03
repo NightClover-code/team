7925f4f82fbb84359bc49ab58fa432cf2ae68682
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-09-03 18:56:55 +0100

    styled footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,34 +4,36 @@ const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <div className="footer__head">
-          <Logo />
-          <p>Collaboration platform for modern teams</p>
-        </div>
-        <div className="footer__body">
-          <ul>
-            <li className="title">Company</li>
-            <li>Product</li>
-            <li>Blog</li>
-            <li>Support</li>
-          </ul>
-          <ul>
-            <li className="title">Features</li>
-            <li>Screen Sharing</li>
-            <li>iOS & Android Apps</li>
-            <li>File Sharing</li>
-            <li>User Management</li>
-          </ul>
-          <ul>
-            <li className="title">Contact Us</li>
-            <li>info@teamapp.com</li>
-            <li>1-888-645-123</li>
-            <li>3500 Sunset BLC, Creek Rd, CA</li>
-          </ul>
-          <ul>
-            <li className="title">Stay up to date</li>
-            <li>Subscribe to our newsletter</li>
-          </ul>
+        <div className="flex">
+          <div className="footer__head">
+            <Logo />
+            <p className="caption">Collaboration platform for modern teams</p>
+          </div>
+          <div className="footer__body">
+            <ul>
+              <li className="title">Company</li>
+              <li className="footer__link">Product</li>
+              <li className="footer__link">Blog</li>
+              <li className="footer__link">Support</li>
+            </ul>
+            <ul>
+              <li className="title">Features</li>
+              <li className="footer__link">Screen Sharing</li>
+              <li className="footer__link">iOS & Android Apps</li>
+              <li className="footer__link">File Sharing</li>
+              <li className="footer__link">User Management</li>
+            </ul>
+            <ul>
+              <li className="title">Contact Us</li>
+              <li className="footer__link">info@teamapp.com</li>
+              <li className="footer__link">1-888-645-123</li>
+              <li className="footer__link">3500 Sunset BLC, Creek Rd, CA</li>
+            </ul>
+            <ul>
+              <li className="title">Stay up to date</li>
+              <li className="footer__link">Subscribe to our newsletter</li>
+            </ul>
+          </div>
         </div>
         <div className="footer__bottom">
           <p className="copyright">&copy; Copyright 2021, TEAM Inc.</p>
