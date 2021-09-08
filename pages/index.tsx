7586ec2d4f5f3d7a253baaa3ac794a33cfe5a2b8
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-09-08 01:40:36 +0100

    homepage unresponsive design complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,30 @@
-//importing components & types
+//importing utils & types
+import { seoConfig } from '../utils';
 import { NextPage } from 'next';
+//importing components
 import Advantages from '../components/Advantages';
 import DashBoard from '../components/Dashboard';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import SEO from '../components/SEO';
 import Testimonials from '../components/Testimonials';
 
 const HomePage: NextPage = () => {
   return (
-    <div className="app__container">
-      <div className="wrapper">
-        <Header />
-        <Hero />
-        <DashBoard />
-        <Advantages />
-        <Testimonials />
-        <Footer />
+    <>
+      <SEO {...seoConfig} />
+      <div className="app__container">
+        <div className="wrapper">
+          <Header />
+          <Hero />
+          <DashBoard />
+          <Advantages />
+          <Testimonials />
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- utils/index.ts
+++ utils/index.ts
@@ -2,5 +2,11 @@ export const isEven = (n: number) => {
   return n % 2 === 0;
 };
 
+export const seoConfig = {
+  title: 'Team - Instant collaboration for remote teams',
+  description:
+    'The easiest and fastest way to share work with groups and to simplify project management.',
+};
+
 export * from './styles';
 export * from './data';
