11850e655e671a7f1d5c642b5bb4893290610f44
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-09-14 17:08:14 +0100

    footer responsivness done

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -10,30 +10,34 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="flex">
-          <div className="footer__head">
-            <Logo />
-            <p className="caption">Collaboration platform for modern teams</p>
+          <div className="flex__no__list">
+            <div className="footer__head">
+              <Logo />
+              <p className="caption">Collaboration platform for modern teams</p>
+            </div>
+            <div className="footer__body">
+              <ul>
+                <li className="title">Company</li>
+                <li className="footer__link">Product</li>
+                <li className="footer__link">Blog</li>
+                <li className="footer__link">Support</li>
+              </ul>
+              <ul>
+                <li className="title">Features</li>
+                <li className="footer__link">Screen Sharing</li>
+                <li className="footer__link">iOS & Android Apps</li>
+                <li className="footer__link">File Sharing</li>
+                <li className="footer__link">User Management</li>
+              </ul>
+              <ul>
+                <li className="title">Contact Us</li>
+                <li className="footer__link">info@teamapp.com</li>
+                <li className="footer__link">1-888-645-123</li>
+                <li className="footer__link">3500 Sunset BLC, Creek Rd, CA</li>
+              </ul>
+            </div>
           </div>
-          <div className="footer__body">
-            <ul>
-              <li className="title">Company</li>
-              <li className="footer__link">Product</li>
-              <li className="footer__link">Blog</li>
-              <li className="footer__link">Support</li>
-            </ul>
-            <ul>
-              <li className="title">Features</li>
-              <li className="footer__link">Screen Sharing</li>
-              <li className="footer__link">iOS & Android Apps</li>
-              <li className="footer__link">File Sharing</li>
-              <li className="footer__link">User Management</li>
-            </ul>
-            <ul>
-              <li className="title">Contact Us</li>
-              <li className="footer__link">info@teamapp.com</li>
-              <li className="footer__link">1-888-645-123</li>
-              <li className="footer__link">3500 Sunset BLC, Creek Rd, CA</li>
-            </ul>
+          <div className="newsletter">
             <ul>
               <li className="title">Stay up to date</li>
               <li className="footer__link special">
