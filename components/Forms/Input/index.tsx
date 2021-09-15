117b104583bb15454264328ac605b63f05d4d8a7
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-09-15 16:38:51 +0100

    fixing mobile nav placeholder issue

--- components/Forms/Input/index.tsx
+++ components/Forms/Input/index.tsx
@@ -1,14 +1,21 @@
-//importing types
-import { CSSProperties } from 'react';
+//importing hooks & types
+import { CSSProperties, useContext } from 'react';
+import { NavContext } from '../../../context';
 
 interface FormInputProps {
   style?: CSSProperties;
 }
 
 const FormInput: React.FC<FormInputProps> = ({ children, style }) => {
+  const { isNavOpen } = useContext(NavContext);
+
   return (
     <div className="form__input">
-      <input placeholder="Email" type="email" style={style} />
+      <input
+        placeholder={isNavOpen ? '' : 'Email'}
+        type="email"
+        style={style}
+      />
       {children}
     </div>
   );
