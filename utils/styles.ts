58e821246ffeeeffd27d3a9ec037d75e9e87ee3f
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-09-15 10:37:24 +0100

    responsivity all done

--- utils/styles.ts
+++ utils/styles.ts
@@ -7,7 +7,7 @@ export const newsletterInputConfig = {
   fontSize: '14px',
   width: '280px',
   padding: '12px 15px',
-  marginTop: '15px',
+  marginTop: '25px',
   backgroundColor: 'rgba(255, 255, 255, 0.1)',
   color: '#fff',
 };
