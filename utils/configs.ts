0032950362491eeaa268f604b15b6ae033e55936
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-09-15 16:00:14 +0100

    adding more utils

--- utils/configs.ts
+++ utils/configs.ts
@@ -2,3 +2,9 @@ export const arrowForwardConfig = {
   width: '30',
   height: '35',
 };
+
+export const seoConfig = {
+  title: 'Team - Instant collaboration for remote teams',
+  description:
+    'The easiest and fastest way to share work with groups and to simplify project management.',
+};
--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,23 @@
+import { SetStateAction, Dispatch } from 'react';
+
 export const isEven = (n: number) => {
   return n % 2 === 0;
 };
 
-export const seoConfig = {
-  title: 'Team - Instant collaboration for remote teams',
-  description:
-    'The easiest and fastest way to share work with groups and to simplify project management.',
+export const hideNav = (setIsNavHidden: Dispatch<SetStateAction<boolean>>) => {
+  window.addEventListener('resize', (e: any) => {
+    if (e.target.innerWidth > 800) {
+      setIsNavHidden(true);
+    }
+  });
+};
+
+export const disableScroll = (isNavHidden: boolean) => {
+  if (!isNavHidden) {
+    document.body.classList.add('disable__scroll');
+  } else {
+    document.body.classList.remove('disable__scroll');
+  }
 };
 
 export * from './styles';
