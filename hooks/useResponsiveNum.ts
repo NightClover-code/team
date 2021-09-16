49bc068c1186ffb9a5d7359ba596c6b39acbd917
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-09-16 14:59:22 +0100

    adding responsive testimonials done

--- hooks/useResponsiveNum.ts
+++ hooks/useResponsiveNum.ts
@@ -2,15 +2,13 @@ import { useEffect, useState } from 'react';
 import { useWindowWidth } from './useWindowWidth';
 
 export const useResponsiveNum = () => {
-  const [responsiveNum, setResponsiveNum] = useState<number>(2);
+  const [responsiveNum, setResponsiveNum] = useState<number>(1);
   const width = useWindowWidth();
 
   useEffect(() => {
     if (width) {
-      if (width > 970) setResponsiveNum(2);
-      else if (670 < width && width < 970) setResponsiveNum(4);
-      else if (400 < width && width < 670) setResponsiveNum(5);
-      else if (width < 400) setResponsiveNum(6);
+      if (width > 970) setResponsiveNum(1);
+      else if (400 < width && width < 970) setResponsiveNum(3);
     }
   }, [width]);
 
